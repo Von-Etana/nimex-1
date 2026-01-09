430450b4017732af947776d52103d29e3dd7199b
Von-Etana <stevietany@gmail.com>
2026-01-09 13:12:07 +0100

    Change base path in Vite config from './' to '/'

--- vite.config.ts
+++ vite.config.ts
@@ -16,7 +16,7 @@ export default defineConfig(({ mode }) => ({
       brotliSize: true,
     }),
   ].filter(Boolean),
-  base: "./",
+  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
